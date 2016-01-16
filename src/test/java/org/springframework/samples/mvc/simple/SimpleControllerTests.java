2ff58b310149168862df8f5572373556e85365fd
zoemak <zoemak@live.com>
2016-01-16 22:38:53 +0800

    测试3

--- src/test/java/org/springframework/samples/mvc/simple/SimpleControllerTests.java
+++ src/test/java/org/springframework/samples/mvc/simple/SimpleControllerTests.java
@@ -1,12 +1,11 @@
 package org.springframework.samples.mvc.simple;
 
+import org.junit.Test;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
-
-import org.junit.Test;
 
 public class SimpleControllerTests {
 
@@ -16,8 +15,7 @@ public class SimpleControllerTests {
 		                                       .perform(get("/simple"))
 		                                       .andExpect(status().isOk())
 		                                       .andExpect(content().contentType("text/plain;charset=ISO-8859-1"))
-		                                       .andExpect(content().string("Hello world!"))
-		                                       .andDo(print());
+		                                       .andExpect(content().string("Hello world!"));
 	}
 
 }
